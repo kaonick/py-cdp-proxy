cf84117f6bf733518947d61eb0710834543d85a2
XDB000/高瑞雲 <N000000930@tw.fpg.com>
2025-03-31 16:56:36 +0800

    20250331

--- cdp_proxy.py
+++ cdp_proxy.py
@@ -63,7 +63,10 @@ def create_remote_browser(port:int=9222,user_profile_path:str=None):
     else:
         chrome_path = args.chrome_path
     # exeFilePath = f'{chrome_path} --remote-debugging-port={port} --incognito -remote-allow-origins=* --user-data-dir={user_profile_path}'
-    exeFilePath = [chrome_path, f'--remote-debugging-port={port}', '--incognito', '-remote-allow-origins=*',f'--user-data-dir={user_profile_path}']
+    exeFilePath = [chrome_path, '--no-sandbox', f'--remote-debugging-port={port}', '--incognito', '-remote-allow-origins=*',f'--user-data-dir={user_profile_path}']
+    # --disable-first-run-ui --no-default-browser-check --no-first-run
+    # --disable-fre ??
+
     try:
         p = subprocess.Popen(exeFilePath)
         print(p.pid) # the pid
